9cd1e91b7252353cf60e739c674567234fa33ccb
Sparsh Agrawal <sparsh.agrawal_ccv18@gla.ac.in>
2020-01-01 14:20:01 +0530

    Selection Sorting Algo

---
+++ src/SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort
+{
+    static void SelectionSort(int[] arr)
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int mi=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j]<arr[mi])
+                    mi=j;
+            }
+            int t=arr[mi];
+            arr[mi]=arr[i];
+            arr[i]=t;
+        }
+    }
+
+    /*
+    public static void main(String[] args)
+    {
+        int[] ar={5,6,7,1,2,3};
+        SelectionSort(ar);
+        for(int i=0;i<ar.length;i++)
+            System.out.print(ar[i]+" ");
+    }
+     */
+
+}
