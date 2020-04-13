346ca39391cdc23df975de19281ed2ffe999f66e
Sparsh-Agrawal <44096686+Sparsh-Agrawal@users.noreply.github.com>
2020-04-13 21:47:20 +0530

    Update SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,12 +1,8 @@
-public class SelectionSort
-{
-    static void selectionSort(int[] arr)
-    {
-        for(int i=0;i<arr.length-1;i++)
-        {
+public class SelectionSort {
+    static void selectionSort(int[] arr) {
+        for(int i=0;i<arr.length-1;i++) {
             int mi=i;
-            for(int j=i+1;j<arr.length;j++)
-            {
+            for(int j=i+1;j<arr.length;j++) {
                 if(arr[j]<arr[mi])
                     mi=j;
             }
@@ -17,13 +13,12 @@ public class SelectionSort
     }
 
     /*
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int[] ar={5,6,7,1,2,3};
         selectionSort(ar);
         for(int i=0;i<ar.length;i++)
             System.out.print(ar[i]+" ");
     }
      */
-
+    
 }
