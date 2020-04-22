5a2df149431c847fae859ccb9016ffe41f218c09
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-22 12:02:41 +0530

    MergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class MergeSort {
 
-    private static void mergeSort(int arr, int low, int high) {
+    private static void mergeSort(int[] arr, int low, int high) {
 
         if(low>=high) {
             return;
@@ -14,7 +16,50 @@ public class MergeSort {
         merge(arr, low, mid, high);
     }
 
-    private static void merge(int arr, int low, int mid, int high) {
+    private static void merge(int[] arr, int low, int mid, int high) {
 
+        int[] brr = new int[high+1];
+        int i = low;
+        int j = mid+1;
+        int k = 0;
+
+        while(i<=mid && j<=high) {
+            if(arr[i] <= arr[j]) {
+                brr[k] = arr[i];
+                k++;
+                i++;
+            }
+            else {
+                brr[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+        if(i>mid) {
+            while(j<=high) {
+                brr[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+        else {
+            while(i<=mid) {
+                brr[k] = arr[i];
+                k++;
+                i++;
+            }
+        }
+
+        k = 0;
+        for(int q = low; q <= high; q++) {
+            arr[q] = brr[k];
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6,4,2,7,3,5,7,3,1};
+        mergeSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 }
