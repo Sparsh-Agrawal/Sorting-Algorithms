a663bf9322af7bbcf7712ac60043b87a1d3e6565
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-22 11:50:42 +0530

    MergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,20 @@
 public class MergeSort {
 
+    private static void mergeSort(int arr, int low, int high) {
+
+        if(low>=high) {
+            return;
+        }
+
+        int mid = (low+high)/2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr,mid+1, high);
+
+        merge(arr, low, mid, high);
+    }
+
+    private static void merge(int arr, int low, int mid, int high) {
+
+    }
 }
