35ba8cab6211dde2926658651e4ae5608bb4420d
Sparsh-Agrawal <agrawal.sparsh76@gmail.com>
2020-04-28 13:50:19 +0530

    HeapSort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,2 +1,28 @@
 public class HeapSort {
+    public static void heapSort(int[] arr){
+        for (int i = 1; i < arr.length ; i++) {
+            int child = i;
+            int parent = (child - 1) / 2;
+            while (parent <= 0 && arr[parent] < arr[child]) {
+                swap(arr, child, parent);
+                child = parent;
+                parent = (child - 1) / 2;
+            }
+        }
+        for (int i = arr.length - 1; i>=0; i--) {
+            swap(arr, i, 0);
+            int parent = 0;
+            int maxChild = getMaxChild(arr, parent, i );
+        }
+    }
+
+    private static int getMaxChild(int[] arr, int parent, int lastSortedIndex) {
+
+    }
+
+    private static void swap(int[] arr, int child, int parent) {
+        int temp = arr[parent];
+        arr[parent] = arr[child];
+        arr[child] = temp;
+    }
 }
